a73d7689bd50b869e0bcd897ce5473934e41e282
Desmond Dadzie <desdad9@gmail.com>
2023-11-03 14:24:18 +0000

    Added a README file that contains all the steps to use this script

---
+++ README.md
@@ -0,0 +1,41 @@
+# Decryptor Script
+
+This Python script allows you to perform decryption using various cryptographic methods including ROT13, Base64, Caesar's Cipher, and Hexadecimal encoding. It provides a menu for selecting the decryption method and prompts for input, making it easy to use.
+
+## Usage
+
+1. Make sure you have Python3 installed on your system.
+
+2. Install the required dependencies by running: <pip install -r requirements.txt>
+
+3. Run the script with the following command: <python3 decryptor.py>
+
+4. Select a decryption method from the menu and, if applicable, enter the Caesar's cipher key.
+
+5. Enter the text to be decrypted or provide a file path (if the text is in a file).
+
+6. The decrypted text will be displayed on the screen.
+
+## Available Decryption Methods
+
+- **ROT13**: A simple letter substitution cipher.
+- **Base64**: Decodes Base64-encoded text.
+- **Caesar's Cipher**: Decrypts text using a specified key.
+- **Hexadecimal**: Decodes hexadecimal-encoded text.
+
+## Requirements
+
+- Python 3
+- Python libraries (specified in requirements.txt)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md).
+
+## Author
+
+- Desmond Dadzie
+
+## Acknowledgments
+
+- Inspiration from playing CTFs and guidance from chatGPT :).
--- decryptor.py
+++ decryptor.py
@@ -1,6 +1,19 @@
 import base64
 
 # ROT13 decryption
+def rot13_encrypt(text):
+    encrypted_text = ""
+    for char in text:
+        if 'a' <= char <= 'z':
+            offset = ord('a')
+            encrypted_text += chr((ord(char) - offset + 13) % 26 + offset)
+        elif 'A' <= char <= 'Z':
+            offset = ord('A')
+            encrypted_text += chr((ord(char) - offset + 13) % 26 + offset)
+        else:
+            encrypted_text += char
+    return encrypted_text
+
 def rot13_decrypt(text):
     return rot13_encrypt(text)  # ROT13 is its own decryption
 
@@ -34,39 +47,43 @@ def hex_decode(text):
     except:
         return "Decoding error. Ensure the input is valid hexadecimal text."
 
-def decrypt(text, method, key=None):
-    if method == "rot13":
-        return rot13_decrypt(text)
-    elif method == "base64":
-        return base64_decode(text)
-    elif method == "caesar":
-        if key is None:
-            return "Key required for Caesar's cipher decryption."
-        return caesar_cipher_decrypt(text, key)
-    elif method == "hex":
-        return hex_decode(text)
-    else:
-        return "Unsupported decryption method."
+def display_menu():
+    print("Decryption Methods:")
+    print("1. ROT13")
+    print("2. Base64")
+    print("3. Caesar's Cipher")
+    print("4. Hexadecimal")
+    method = input("Select a decryption method (1/2/3/4): ")
+    return method
 
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) != 4:
-        print("Usage: python decryptor.py <method> <key (for Caesar's cipher)> <text or filename>")
-        sys.exit(1)
-
-    method = sys.argv[1]
-    key = int(sys.argv[2]) if method == "caesar" else None
-    input_text = sys.argv[3]
-
+def decrypt(method, key=None):
+    input_text = input("Enter the text or the filename: ")
     if input_text.lower().endswith(".txt"):
         try:
             with open(input_text, "r") as file:
                 input_text = file.read()
         except Exception as e:
-            print(f"Error reading file: {e}")
-            sys.exit(1)
+            return f"Error reading file: {e}"
 
-    result = decrypt(input_text, method, key)
+    if method == "1":
+        return rot13_decrypt(input_text)
+    elif method == "2":
+        return base64_decode(input_text)
+    elif method == "3":
+        if key is None:
+            return "Key required for Caesar's cipher decryption."
+        return caesar_cipher_decrypt(input_text, key)
+    elif method == "4":
+        return hex_decode(input_text)
+    else:
+        return "Unsupported decryption method."
+
+if __name__ == "__main__":
+    method = display_menu()
+    key = None
+    if method == "3":
+        key = int(input("Enter the Caesar's cipher key: "))
+    
+    result = decrypt(method, key)
     print("Decrypted text:")
     print(result)
